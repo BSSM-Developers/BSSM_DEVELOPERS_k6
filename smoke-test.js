57cc2f210698a1f24ac87125e18f3247d32f70c4
pdh0128 <pdh0128a@gmail.com>
2025-12-16 19:11:45 +0900

    smoke test

---
+++ smoke-test.js
@@ -0,0 +1,23 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+const BASE_URL = 'https://dev.bssm-dev.com/api';
+
+export const options = {
+  vus: 1,
+  duration: '30s',
+};
+
+export default function () {
+  const headers = {
+    'bssm-dev-token': __ENV.BSSM_DEVELOPERS_CLIENT_ID,
+    'bssm-dev-secret': __ENV.BSSM_DEVELOPERS_SECRET_KEY,
+  };
+
+  const res = http.get(`${BASE_URL}/proxy-server/posts`, { headers });
+  check(res, {
+    'status is 200': (r) => r.status === 200,
+  });
+  sleep(1);
+}
+
